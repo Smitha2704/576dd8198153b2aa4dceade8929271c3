3ff70e831358fbf29b3a0f1503b5bfd15e692b92
Smitha2704 <69708426+Smitha2704@users.noreply.github.com>
2021-07-09 17:36:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -41,23 +41,7 @@ function draw(){
     background(0); 
 
     //creating thunder
-    rand = Math.round(random(1,4));
-    if(frameCount%80===0){
-        thunderCreatedFrame=frameCount;
-        thunder = createSprite(random(10,370), random(10,30), 10, 10);
-        switch(rand){
-            case 1: thunder.addImage(thunder1);
-            break;
-            case 2: thunder.addImage(thunder2);
-            break; 
-            case 3: thunder.addImage(thunder3);
-            break;
-            case 4: thunder.addImage(thunder4);
-            break;
-            default: break;
-        }
-        thunder.scale = random(0.3,0.6)
-    }
+  
 
     if(thunderCreatedFrame + 10 ===frameCount && thunder){
         thunder.destroy();
